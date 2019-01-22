4473166b11edb5d9aea91f97a2ca0e4a2e16e928
汪洋 <yang.wang11@ucarinc.com>
2019-01-22 18:23:30 +0800

    L5 动态规划和Manacher算法

--- src/main/java/question/Leetcode5M.java
+++ src/main/java/question/Leetcode5M.java
@@ -1,5 +1,6 @@
 package question;
 
+import com.sun.javafx.collections.MapListenerHelper;
 import com.sun.xml.internal.bind.v2.model.core.ID;
 
 /**
@@ -20,7 +21,7 @@ import com.sun.xml.internal.bind.v2.model.core.ID;
 public class Leetcode5M {
 
     //中心扩展法
-    public static String longestPalindrome(String s) {
+    public static String longestPalindrome1(String s) {
         if (s == null || s.length() == 0){
             return s;
         }
@@ -53,7 +54,52 @@ public class Leetcode5M {
         return s.substring(start, end + 1);
     }
 
+    //动态规划
+    public static String longestPalindrome2(String s) {
+        if (s == null || s.length() == 0){
+            return s;
+        }
+        char[] arr = s.toCharArray();
+        boolean [] lastMap = new boolean [arr.length];
+        boolean [] map = new boolean [arr.length];
+        int start = 0;
+        int end = 0;
+        for(int i = arr.length-1 ; i >= 0; i-- ){
+            for (int j = i ; j<arr.length; j++){
+                if (isPalindrome(i,j,arr,lastMap)){
+                    map[j] = true;
+                    if(j-i>end-start){
+                        end = j;
+                        start = i;
+                    }
+                }
+            }
+            lastMap = map;
+            map = new boolean [arr.length];
+        }
+        return s.substring(start, end + 1);
+    }
+
+    private static boolean isPalindrome(int i, int j,char[] arr, boolean [] map){
+        if (j-i == 0) return true;
+        if (j-i == 1) return arr[i]==arr[j];
+        return map[j-1] && arr[i]==arr[j];
+    }
+
+    //Manacher算法
+    public static String longestPalindrome(String s) {
+        char[] news =new char[s.length()*2+1];
+        for (int i =0 ;i <s.length()*2+1;i++){
+            if (i % 2 ==0) {
+                news[i] = '*';
+            }else {
+                news[i] = s.charAt((i-1)/2);
+            }
+        }
+        int[] radius = new int[news.length];
+
+    }
     public static void main(String[] args) {
-        longestPalindrome("bb");
+        longestPalindrome("babad");
     }
 }
